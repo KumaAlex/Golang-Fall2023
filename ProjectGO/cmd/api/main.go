4c061c41f21008a90a7034a28ea13e02f2d7792c
Alex Kim <kumaalexbear@gmail.com>
2023-11-04 16:59:21 +0600

    Chapter 10 completed. structured jsonlogging

--- ProjectGO/cmd/api/main.go
+++ ProjectGO/cmd/api/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"ProjectGO/internal/data"
+	"ProjectGO/internal/jsonlog"
 	"context"
 	"database/sql"
 	"flag"
 	"fmt"
 	_ "github.com/lib/pq"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -28,7 +28,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -43,23 +43,21 @@ func main() {
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
-	}
 
+		logger.PrintFatal(err, nil)
+	}
 	defer db.Close()
 
-	logger.Printf("database connection pool established")
-
+	logger.PrintInfo("database connection pool established", nil)
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
 	}
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -67,10 +65,14 @@ func main() {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
--- ProjectGO/cmd/api/routes.go
+++ ProjectGO/cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
@@ -20,19 +20,6 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/laptopBags/:id", app.updateLaptopBagHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/laptopBags/:id", app.deleteLaptopBagHandler)
 
-	return router
+	return app.recoverPanic(router)
+
 }
-
-//BODY='{"brand":"Urban lifestyle","color":"black","weight":"1.6 kg", "Dimensions":[10,16,41.6]}'
-//curl -i -d "$BODY" localhost:4000/v1/laptopBags
-//BODY='{"brand":"Pandec","color":"black and white","weight":"4 kg", "Dimensions":[40, 40, 40]}'
-//curl -i -d "$BODY" localhost:4000/v1/laptopBags
-//BODY='{"brand":"Nig","color":"black","weight":"0.1 kg", "Dimensions":[4,5,6]}'
-//curl -i -d "$BODY" localhost:4000/v1/laptopBags
-
-//BODY='{"brand":"Urban lifestyle","color":"black","weight":"69 kg", "Dimensions":[10,16,41.6]}'
-//curl -X PUT -d "$BODY" localhost:4000/v1/laptopBags/1
-
-//curl localhost:4000/v1/laptopBags/1
-
-//curl -X DELETE localhost:4000/v1/laptopBags/3
